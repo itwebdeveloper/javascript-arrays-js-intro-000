e77fb4cde8a526597c5d3bbe0467c1d4ede4ce2d
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 14:43:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,5 @@ function addElementToBeginningOfArray(arr, array_element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, array_element) {
-  return arr.unshift(array_element);
+  arr.unshift(array_element);
 }
